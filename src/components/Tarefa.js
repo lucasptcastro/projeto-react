44db9489497879e1870b0914c87a30f6ff5b95f8
lucasptcastro <lucaspeixoto10.001@gmail.com>
2022-07-22 11:47:41 -0300

    implementando css

--- src/components/Tarefa.js
+++ src/components/Tarefa.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import './Tarefa.css';
+
 const Tarefa = ({ tarefa }) => {
-    return <h1>{tarefa.title}</h1>;
+    return <div className='tarefa-container'>{tarefa.title}</div>;
 };
  
 export default Tarefa;
